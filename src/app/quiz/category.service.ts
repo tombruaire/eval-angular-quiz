b259618ccc7348bf06ef5b96ed81a1313957bd0f
Tom BRUAIRE <tombruaire@gmail.com>
2024-10-16 11:48:56 +0200

    Affichage des catégories dynamiquement

--- src/app/quiz/category.service.ts
+++ src/app/quiz/category.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  categoryContent: any[] = [];
+
+  getCategoryContent() {
+    this.http.get('http://localhost:3000/category').subscribe((category: any) => {
+      for (const categorie of category) {
+        this.categoryContent.push({
+          id: categorie.id,
+          categorie: categorie.libelle,
+        });
+      }
+    });
+  }
+
+  resetCategory() {
+    this.categoryContent = [];
+  }
+
 }
--- src/app/quiz/category/category.component.ts
+++ src/app/quiz/category/category.component.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {CategoryService} from "../category.service";
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
   constructor(
     private router: Router,
-  ) {
-  }
+    private categoryService: CategoryService
+  ) { }
+
+  categoryContent: any[] = this.categoryService.categoryContent;
 
   playerName = '';
   categoryId = null;
 
   navigateToQuiz() {
     this.router.navigate(['/quiz', this.categoryId, this.playerName]);
+  }
+
+  ngOnInit(): void {
+    this.categoryService.getCategoryContent();
   }
 }
