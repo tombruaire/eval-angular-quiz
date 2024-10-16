f847edcf25b3fb984ce31ff60f9772f78697064d
Tom BRUAIRE <tombruaire@gmail.com>
2024-10-16 11:08:16 +0200

    Modification du navigate

--- src/app/quiz/category/category.component.ts
+++ src/app/quiz/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-category',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent {
+  constructor(
+    private router: Router,
+  ) {
+  }
 
+  playerName = '';
+  categoryId = null;
+
+  navigateToQuiz() {
+    this.router.navigate(['/quiz', this.categoryId, this.playerName]);
+  }
 }
